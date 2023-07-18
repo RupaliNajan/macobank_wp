31f081420382c3a8761055b2a00effd68072217c
Rupali Najan <Rupali.Najan@esds.co.in>
2023-07-18 12:32:14 +0530

    page changes upgradation

---
+++ wp-content/themes/front/ATM_POS_ECOM.php
@@ -0,0 +1,58 @@
+<?php
+/*Template Name:ATM / POS / ECOM */
+get_header();
+?>
+
+<div class="container">
+ <h2 style="color: #900C3F"><b><?php the_title();?></b></h2>
+	<h3 style="color: green"><center>ATM Service</center></h3>
+    <ul >
+        <li>The Maharashtra Mantralaya & Allied Offices Co-Operative Bank Ltd, MUMBAI, Rupay ATM withdraw money anytime anywhere ATM in India
+        </li>
+        <li>You do not need to spend time waiting in queues at the branch to perform your transactions. An ATM provides you with most of the banking services. What’s more, you can use the Maco Bank ATM Card over 170,000 NFS/BANKS shared ATMs in India.</li>
+        <li>Save Time : By banking at the ATM, you save time spent on traveling to the branch or waiting for a teller.</li>
+        <li>24x7 banking : At the ATM, you don't need to worry about reaching before closing time and banking on holidays.</li>
+        <li>Security Features : When your bank at the ATM, you can make the most of its secure features like daily transaction limits and ATM receipts that help you keep track of your monies.</li>
+        <li>Daily Transaction Limits : You can withdraw up to the following amounts per day from the ATM depending on your account</li>
+    </ul>
+
+    <table class="table table-striped">
+    	<thead>
+    		<td>Transaction Type</td>
+    		<td>Per Transaction Limit</td>
+    		<td>If Per Day Limit</td>
+    	</thead>
+		<tbody>
+			<tr>
+				<td>RuPay Debit Card + ECOM</td>
+				<td>NA</td>
+                <td>50,000</td>
+			</tr>
+			<tr>
+				<td>Issuing Transactions</td>
+				<td>15,000</td>
+                <td>20,000</td>
+			</tr>
+			<tr>
+				<td>Acquirer Transaction</td>
+				<td>10,000</td>
+                <td>NA (Limit defined by acquirer bank)</td>
+			</tr>
+			<tr>
+				<td>Total Limit</td>
+				<td></td>
+                <td>70,000</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<h4>ATM Complaint</h4>
+	<ul>
+		<li>Customer's complaint on failed transactions to be resolved by bank within 7 working days from the date of receipt of complaint. For more details please log on to www.rbi.org.in notification DPSS.PD.No.2632 / 02.10.002 / 2010-2011</li>
+		<li>ATM Helpline number: 022-62664100.</li>
+	</ul><br>
+</div>
+
+<?php
+get_footer();
+?>
